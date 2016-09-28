f305c3c5932420a957c4e33bf60d8f24f0a4da64
Haytham Allos <haytham.allos@gmail.com>
2016-09-27 22:28:46 -0700

    .

--- VetAppSolution/VetWebApp/Controllers/AccountController.cs
+++ VetAppSolution/VetWebApp/Controllers/AccountController.cs
@@ -18,75 +18,11 @@ namespace VetWebApp.Controllers
 {
     public class AccountController : Controller
     {
-        private readonly Auth0Settings _auth0Settings;
-
-        public AccountController(IOptions<Auth0Settings> auth0Settings)
+        public IActionResult Login()
         {
-            _auth0Settings = auth0Settings.Value;
+            return new ChallengeResult("Auth0", new AuthenticationProperties() { RedirectUri = "/" });
         }
 
-        [HttpGet]
-        public IActionResult Login(string returnUrl = "/")
-        {
-            ViewData["ReturnUrl"] = returnUrl;
-            return View();
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> Login(LoginViewModel vm, string returnUrl = null)
-        {
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    AuthenticationApiClient client = new AuthenticationApiClient(new Uri($"https://{_auth0Settings.Domain}/"));
-
-                    var result = await client.AuthenticateAsync(new AuthenticationRequest
-                    {
-                        ClientId = _auth0Settings.ClientId,
-                        Scope = "openid",
-                        Connection = "vetdbdev", // Specify the correct name of your DB connection
-                        Username = vm.EmailAddress,
-                        Password = vm.Password
-                    });
-
-                    // Get user info from token
-                    var user = await client.GetTokenInfoAsync(result.IdToken);
-
-                    // Create claims principal
-                    var claimsPrincipal = new ClaimsPrincipal(new ClaimsIdentity(new[]
-                    {
-                        new Claim(ClaimTypes.NameIdentifier, user.UserId),
-                        new Claim(ClaimTypes.Name, user.FullName)
-
-                    }, CookieAuthenticationDefaults.AuthenticationScheme));
-
-                    // Sign user into cookie middleware
-                    await HttpContext.Authentication.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, claimsPrincipal);
-
-                    return RedirectToLocal(returnUrl);
-                }
-                catch (Exception e)
-                {
-                    ModelState.AddModelError("", e.Message);
-                }
-            }
-
-            return View(vm);
-        }
-
-        [HttpGet]
-        public IActionResult LoginExternal(string connection, string returnUrl = "/")
-        {
-            var properties = new AuthenticationProperties() { RedirectUri = returnUrl };
-
-            if (!string.IsNullOrEmpty(connection))
-                properties.Items.Add("connection", connection);
-
-            return new ChallengeResult("Auth0", properties);
-        }
-
-        [Authorize]
         public IActionResult Logout()
         {
             HttpContext.Authentication.SignOutAsync("Auth0");
@@ -95,31 +31,114 @@ namespace VetWebApp.Controllers
             return RedirectToAction("Index", "Home");
         }
 
-        /// <summary>
-        /// This is just a helper action to enable you to easily see all claims related to a user. It helps when debugging your
-        /// application to see the in claims populated from the Auth0 ID Token
-        /// </summary>
-        /// <returns></returns>
-        [Authorize]
-        public IActionResult Claims()
-        {
-            return View();
-        }
+        //private readonly Auth0Settings _auth0Settings;
 
-        #region Helpers
+        //public AccountController(IOptions<Auth0Settings> auth0Settings)
+        //{
+        //    _auth0Settings = auth0Settings.Value;
+        //}
 
-        private IActionResult RedirectToLocal(string returnUrl)
-        {
-            if (Url.IsLocalUrl(returnUrl))
-            {
-                return Redirect(returnUrl);
-            }
-            else
-            {
-                return RedirectToAction(nameof(HomeController.Index), "Home");
-            }
-        }
+        //[HttpGet]
+        //public IActionResult Login(string returnUrl = "/")
+        //{
+        //    ViewData["ReturnUrl"] = returnUrl;
+        //    return View();
+        //}
+        //[HttpGet]
+        //public IActionResult Register(string returnUrl = "/")
+        //{
+        //    ViewData["ReturnUrl"] = returnUrl;
+        //    return View();
+        //}
+        //[HttpPost]
+        //public async Task<IActionResult> Login(LoginViewModel vm, string returnUrl = null)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        try
+        //        {
+        //            AuthenticationApiClient client = new AuthenticationApiClient(new Uri($"https://{_auth0Settings.Domain}/"));
 
-        #endregion
+        //            var result = await client.AuthenticateAsync(new AuthenticationRequest
+        //            {
+        //                ClientId = _auth0Settings.ClientId,
+        //                Scope = "openid",
+        //                Connection = "vetdbdev", // Specify the correct name of your DB connection
+        //                Username = vm.EmailAddress,
+        //                Password = vm.Password
+        //            });
+
+        //            // Get user info from token
+        //            var user = await client.GetTokenInfoAsync(result.IdToken);
+
+        //            // Create claims principal
+        //            var claimsPrincipal = new ClaimsPrincipal(new ClaimsIdentity(new[]
+        //            {
+        //                new Claim(ClaimTypes.NameIdentifier, user.UserId),
+        //                new Claim(ClaimTypes.Name, user.FullName)
+
+        //            }, CookieAuthenticationDefaults.AuthenticationScheme));
+
+        //            // Sign user into cookie middleware
+        //            await HttpContext.Authentication.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, claimsPrincipal);
+
+        //            return RedirectToLocal(returnUrl);
+        //        }
+        //        catch (Exception e)
+        //        {
+        //            ModelState.AddModelError("", e.Message);
+        //        }
+        //    }
+
+        //    return View(vm);
+        //}
+
+
+        //[HttpGet]
+        //public IActionResult LoginExternal(string connection, string returnUrl = "/")
+        //{
+        //    var properties = new AuthenticationProperties() { RedirectUri = returnUrl };
+
+        //    if (!string.IsNullOrEmpty(connection))
+        //        properties.Items.Add("connection", connection);
+
+        //    return new ChallengeResult("Auth0", properties);
+        //}
+
+        //[Authorize]
+        //public IActionResult Logout()
+        //{
+        //    HttpContext.Authentication.SignOutAsync("Auth0");
+        //    HttpContext.Authentication.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+
+        //    return RedirectToAction("Index", "Home");
+        //}
+
+        ///// <summary>
+        ///// This is just a helper action to enable you to easily see all claims related to a user. It helps when debugging your
+        ///// application to see the in claims populated from the Auth0 ID Token
+        ///// </summary>
+        ///// <returns></returns>
+        //[Authorize]
+        //public IActionResult Claims()
+        //{
+        //    return View();
+        //}
+
+        //#region Helpers
+
+        //private IActionResult RedirectToLocal(string returnUrl)
+        //{
+        //    if (Url.IsLocalUrl(returnUrl))
+        //    {
+        //        return Redirect(returnUrl);
+        //    }
+        //    else
+        //    {
+        //        return RedirectToAction(nameof(HomeController.Index), "Home");
+        //    }
+        //}
+
+        //#endregion
     }
 }
--- VetAppSolution/VetWebApp/Startup.cs
+++ VetAppSolution/VetWebApp/Startup.cs
@@ -41,8 +41,8 @@ namespace VetWebApp
         public void ConfigureServices(IServiceCollection services)
         {
             // Add authentication services
-            services.AddAuthentication(
-                options => options.SignInScheme = CookieAuthenticationDefaults.AuthenticationScheme);
+            //services.AddAuthentication(
+            //    options => options.SignInScheme = CookieAuthenticationDefaults.AuthenticationScheme);
 
             services.AddMvc();
 
@@ -50,12 +50,12 @@ namespace VetWebApp
             services.AddOptions();
 
             // Add the Auth0 Settings object so it can be injected
-            services.Configure<Auth0Settings>(Configuration.GetSection("Auth0"));
+            //services.Configure<Auth0Settings>(Configuration.GetSection("Auth0"));
 
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory, IOptions<Auth0Settings> auth0Settings)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
             //loggerFactory.AddConsole();
 
@@ -68,60 +68,60 @@ namespace VetWebApp
             app.UseStatusCodePagesWithReExecute("/StatusCode/{0}");
             app.UseStaticFiles();
 
-            // Add the cookie middleware
-            app.UseCookieAuthentication(new CookieAuthenticationOptions
-            {
-                AutomaticAuthenticate = true,
-                AutomaticChallenge = true
-            });
+            //// Add the cookie middleware
+            //app.UseCookieAuthentication(new CookieAuthenticationOptions
+            //{
+            //    AutomaticAuthenticate = true,
+            //    AutomaticChallenge = true
+            //});
 
-            // Add the OIDC middleware
-            app.UseOpenIdConnectAuthentication(new OpenIdConnectOptions("Auth0")
-            {
-                // Set the authority to your Auth0 domain
-                Authority = $"https://{auth0Settings.Value.Domain}",
+            //// Add the OIDC middleware
+            //app.UseOpenIdConnectAuthentication(new OpenIdConnectOptions("Auth0")
+            //{
+            //    // Set the authority to your Auth0 domain
+            //    Authority = $"https://{auth0Settings.Value.Domain}",
 
-                // Configure the Auth0 Client ID and Client Secret
-                ClientId = auth0Settings.Value.ClientId,
-                ClientSecret = auth0Settings.Value.ClientSecret,
+            //    // Configure the Auth0 Client ID and Client Secret
+            //    ClientId = auth0Settings.Value.ClientId,
+            //    ClientSecret = auth0Settings.Value.ClientSecret,
 
-                // Do not automatically authenticate and challenge
-                AutomaticAuthenticate = false,
-                AutomaticChallenge = false,
+            //    // Do not automatically authenticate and challenge
+            //    AutomaticAuthenticate = false,
+            //    AutomaticChallenge = false,
 
-                // Set response type to code
-                ResponseType = "code",
+            //    // Set response type to code
+            //    ResponseType = "code",
 
-                // Set the callback path, so Auth0 will call back to http://localhost:5000/signin-auth0 
-                // Also ensure that you have added the URL as an Allowed Callback URL in your Auth0 dashboard 
-                CallbackPath = new PathString("/signin-auth0"),
+            //    // Set the callback path, so Auth0 will call back to http://localhost:5000/signin-auth0 
+            //    // Also ensure that you have added the URL as an Allowed Callback URL in your Auth0 dashboard 
+            //    CallbackPath = new PathString("/signin-auth0"),
 
-                // Configure the Claims Issuer to be Auth0
-                ClaimsIssuer = "Auth0",
-                Events = new OpenIdConnectEvents
-                {
-                    OnRedirectToIdentityProvider = context =>
-                    {
-                        if (context.Properties.Items.ContainsKey("connection"))
-                            context.ProtocolMessage.SetParameter("connection", context.Properties.Items["connection"]);
+            //    // Configure the Claims Issuer to be Auth0
+            //    ClaimsIssuer = "Auth0",
+            //    //Events = new OpenIdConnectEvents
+            //    //{
+            //    //    OnRedirectToIdentityProvider = context =>
+            //    //    {
+            //    //        if (context.Properties.Items.ContainsKey("connection"))
+            //    //            context.ProtocolMessage.SetParameter("connection", context.Properties.Items["connection"]);
 
-                        return Task.FromResult(0);
-                    }
-                }
-
-
-            });
-
-            app.UseMvc(routes =>
-            {
-                routes.MapRoute(
-                    name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
-            });
+            //    //        return Task.FromResult(0);
+            //    //    }
+            //    //}
 
 
+            //});
 
-            //app.UseMvcWithDefaultRoute();
+            //app.UseMvc(routes =>
+            //{
+            //    routes.MapRoute(
+            //        name: "default",
+            //        template: "{controller=Home}/{action=Index}/{id?}");
+            //});
+
+
+
+            app.UseMvcWithDefaultRoute();
             //app.UseMvc(
             //    routes =>
             //    {
